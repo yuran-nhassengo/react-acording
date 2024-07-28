fee728fb4109a642d8e79436c229840223ff9b3f
yuran-nhassengo <yurannhassengo@gmail.com>
2024-07-28 15:06:13 +0300

    Finalize styling

--- src/components/according/index.jsx
+++ src/components/according/index.jsx
@@ -9,20 +9,24 @@ export const Accordian = () => {
     const [selected,setSelected] = useState(null);
 
     function handleSingleSelection (getCurrentId){
-        console.log(getCurrentId)
+        setSelected(getCurrentId === selected ? null : getCurrentId)
     }
 
 
   return (
-    <div>
-      <div className='text-center'>
+    <div className='flex justify-center items-center h-screen bg-gray-200'>
+      <div className='w-full max-w-screen-md text-center'>
         {
             data && data.length > 0 ?
-             data.map(dataItem => <div>
-                <div onClick={()=>handleSingleSelection(dataItem.id)}>
-                    <h3>{dataItem.pergunta}</h3>
-                    <span>+</span>
+             data.map(dataItem => <div className=" border border-blue-400 rounded-lg shadow-md mb-4 p-4 " key={dataItem.id}>
+                <div className="cursor-pointer flex justify-between items-center"  onClick={()=>handleSingleSelection(dataItem.id)}>
+                    <h3 className="text-lg font-semibold">{dataItem.pergunta}</h3>
+                    <span className="text-xl">+</span>
                 </div>
+                {
+                  selected === dataItem.id ? <div className="mt-2"><p>{dataItem.resposta}</p></div> 
+                  :null
+                }
              </div>)
              : <div>No data Found!</div>
         }
